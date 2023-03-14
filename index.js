aff92f95248734ef30688b440098c47c720031f7
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-03-14 15:31:11 +0530

    darkmode,joinedDate

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// mode
+const btnmode=document.getElementById("btn-mode");
+const modetext=document.getElementById("mode-text");
+const modeicon=document.getElementById("mode-icon");
+
+const root = document.documentElement.style;
+
 //serach bar
 const input=document.getElementById("input");
 const noresults=document.getElementById("no-result");
@@ -17,6 +24,8 @@ const page=document.getElementById("page");
 const twitter=document.getElementById("twitter");
 const company=document.getElementById("company");
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let darkMode=false;
+let dates_breakdown;
 
 //api
 const url = "https://api.github.com/users/";
@@ -48,6 +57,14 @@ input.addEventListener("input",()=>{
     noresults.style.display="none";
 
 });
+
+btnmode.addEventListener("click",()=>{
+    if(darkMode==false){
+        darkModeProperties();
+    }else{
+        lightModeProperties();
+    }
+})
 
 
 
@@ -89,6 +106,10 @@ function updateProfile(data){
         avatar.src=`${data.avatar_url}`;
     
         userName.innerText=data.name===null ?data.login : data.name;
+
+        dates_breakdown=data.created_at.split("T").shift().split("-");
+
+        date.innerText=`Joined ${dates_breakdown[2]} ${months[dates_breakdown[1]-1]} ${dates_breakdown[0]}`;
     
         user.innerText=`@${data.login}`;
     
@@ -118,3 +139,55 @@ function updateProfile(data){
         noresults.style.display="block";
     }
 }
+
+function init(){
+    darkMode=false;
+
+    const prefersDarkMode=window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+
+    const localStorageDarkMode=localStorage.getItem("dark-mode");
+
+    if(localStorageDarkMode===null){
+        localStorage.setItem("dark-mode",prefersDarkMode);
+        darkMode=prefersDarkMode;
+    }
+
+    if(localStorageDarkMode){
+        darkMode=localStorageDarkMode;
+        darkModeProperties();
+    }else{
+        localStorage.setItem("dark-mode",prefersDarkMode);
+        darkMode=prefersDarkMode;
+        lightModeProperties();
+    }
+    getUserData(url+"Apoorv-cloud");
+}
+
+function darkModeProperties() {
+    root.setProperty("--lm-bg", "#141D2F");
+    root.setProperty("--lm-bg-content", "#1E2A47");
+    root.setProperty("--lm-text", "white");
+    root.setProperty("--lm-text-alt", "white");
+    root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+    modetext.innerText = "LIGHT";
+    modeicon.src = "./assets/images/sun-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(1000%)");
+    darkMode = true;
+    localStorage.setItem("dark-mode", true);
+  }
+  function lightModeProperties() {
+    root.setProperty("--lm-bg", "#F6F8FF");
+    root.setProperty("--lm-bg-content", "#FEFEFE");
+    root.setProperty("--lm-text", "#4B6A9B");
+    root.setProperty("--lm-text-alt", "#2B3442");
+    root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+    modetext.innerText = "DARK";
+    modeicon.src = "./assets/images/moon-icon.svg";
+    root.setProperty("--lm-icon-bg", "brightness(100%)");
+    darkMode = false;
+    localStorage.setItem("dark-mode", false);
+  }
+
+
+init();
